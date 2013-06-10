6653179aa906d4c85613e995d9b9e6622a18b465
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2013-06-10 07:16:05 -0400

    New app (initial commit)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FurryHelpster::Application.config.secret_token = 'aede28ec3c2ff8d515b9a3d304b6a29b274763f4d2fdc7ec58f08bf596454c08f7aed12df807f442fb2292b8ac93987fb05e18d3bdc430370cfb092e39611d77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FurryHelpster::Application.config.session_store :cookie_store, key: '_furry-helpster_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FurryHelpster::Application.config.session_store :active_record_store
