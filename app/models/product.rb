78db36dc4cc1b2099199611bb39d53c9da766532
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2013-06-14 22:02:39 -0400

    Add a current_version method to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :private
 
   scope :all_public, where(:private => false)
+
+  def current_version
+    Version.where(:product_id => self.id, :state => :current).first
+  end
 end
